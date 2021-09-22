224058a878b448ce411a4ccc90b63e48e5498c28
gmzhai <gmzhai@tencent.com>
2021-09-22 17:22:47 +0800

    init

---
+++ components/hello-world/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HelloWorld() {
+  return <h2>Hello World</h2>;
+}
+
+export default HelloWorld;
---
+++ projects/app1/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import HelloWorld from "co-hello-world";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>app1</h1>
+        <HelloWorld />
+      </header>
+    </div>
+  );
+}
+
+export default App;
